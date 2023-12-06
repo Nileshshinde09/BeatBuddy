70dd203341ec0cf555caaf4fed57605f903ebdb9
Nilesh Shinde <90444068+Nileshshinde09@users.noreply.github.com>
2023-12-06 19:08:47 +0530

    Update login.jsx

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -28,7 +28,6 @@ const Login = () => {
                 if (userData) {
                     dispatch(authLogin(userData))
                 }
-                console.log(userData)
                 navigate("/")
             }
         } catch (error) {

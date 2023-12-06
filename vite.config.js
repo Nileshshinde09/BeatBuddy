4f934846bd01259c8501889631bef4469f5a67fc
Nilesh Shinde <90444068+Nileshshinde09@users.noreply.github.com>
2023-12-06 11:14:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
-      '/api':'http://127.0.0.1:5000',
+      '/api':'https://beatbuddybackend.onrender.com',
     }
   }
 })

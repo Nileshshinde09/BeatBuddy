9487bbe9115f6d0aa436f3d27cccd491f350ebce
Nilesh Shinde <90444068+Nileshshinde09@users.noreply.github.com>
2023-12-06 12:54:48 +0530

    Update conf.js

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,6 +4,7 @@ const conf={
     appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBuckedId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    backendURL:String(import.meta.env.VITE_BACKEND_URL),
 }
 
 export default conf;

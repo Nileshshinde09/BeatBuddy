8674c1a0e547f41e4283a27b4e3b40c90dc1fdb5
Nilesh Shinde <90444068+Nileshshinde09@users.noreply.github.com>
2023-12-10 17:14:24 +0530

    Update register.jsx

--- src/components/Register/register.jsx
+++ src/components/Register/register.jsx
@@ -30,6 +30,7 @@ const Signup = () => {
                 if (userData) {
                     dispatch(login(userData));
                 }
+                console.log(userData)
                 navigate("/")
             }
         } catch (error) {
@@ -61,7 +62,7 @@ const Signup = () => {
                                         </div>
                                     </h1>
                                     <div>
-                                        <img src='home.png' className='max-sm:h-[15rem] sm:h-[20rem] sm:mt-[1rem] min-lg:h-[25rem] drop-shadow-xl'>
+                                        <img src='public/home.png' className='max-sm:h-[15rem] sm:h-[20rem] sm:mt-[1rem] min-lg:h-[25rem] drop-shadow-xl'>
                                         </img>
                                     </div>
                                 </div>
@@ -69,11 +70,11 @@ const Signup = () => {
                         </div>
                     </div>
                     <div>
-                        <section class="bg-gray-50 dark:bg-gray-900 w-[30rem]">
-                            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        <section className=" bg-gray-900 sm:h-[20rem]">
+                            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                                <div className="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                                        <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
                                             Create account
                                         </h1>
                                         <p className="mt-2 text-center text-base text-white">
@@ -86,19 +87,19 @@ const Signup = () => {
                                             </Link>
                                         </p>
                                         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-                                        <form class="space-y-4 md:space-y-6" onSubmit={handleSubmit(create)}>
+                                        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(create)}>
 
                                             <div>
-                                                <label for="fname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Full Name</label>
-                                                <input type="text" name="fname" id="fname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="example" required=""
+                                                <label for="fname" className="block mb-2 text-sm font-medium text-white">Your Full Name</label>
+                                                <input type="text" name="fname" id="fname" className="bg-gray-50 border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="example" required=""
                                                     {...register("name", {
                                                         required: true,
                                                     })}
                                                 />
                                             </div>
                                             <div>
-                                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""
+                                                <label for="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
+                                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""
                                                     {...register("email", {
                                                         required: true,
                                                         validate: {
@@ -109,19 +110,19 @@ const Signup = () => {
                                                 />
                                             </div>
                                             <div>
-                                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Create Password</label>
-                                                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""
+                                                <label for="password" className="block mb-2 text-sm font-medium text-white">Create Password</label>
+                                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""
                                                     {...register("password", {
                                                         required: true,
                                                     })}
                                                 />
                                             </div>
 
-                                            <div class="flex items-start">
+                                            <div className="flex items-start">
                                                 
                                                
                                             </div>
-                                            <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
                                         </form>
                                     </div>
                                 </div>

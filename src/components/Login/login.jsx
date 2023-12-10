b03ff7c16e480dc7572fdf96777e3c14ec685be7
Nilesh Shinde <90444068+Nileshshinde09@users.noreply.github.com>
2023-12-10 17:05:14 +0530

    Update login.jsx

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -69,12 +69,12 @@ const Login = () => {
                         </div>
                     </div>
                     <div>
-                        <section className="bg-gray-50 sm:w-[30rem] dark:bg-gray-900">
+                         <section className=" sm:w-[30rem] dark:bg-gray-900">
                             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
 
-                                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                                <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
                                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                        <h1 className="text-xl font-bold leading-tight tracking-tight  md:text-2xl text-white">
                                             Sign in to your account
                                         </h1>
                                         <p className="mt-2 text-center text-base text-white">

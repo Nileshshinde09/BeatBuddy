cbc1f04b018e116de588810f411392c0ccbd0d6a
Nilesh Shinde <90444068+Nileshshinde09@users.noreply.github.com>
2023-12-10 17:27:20 +0530

    Update login.jsx

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -91,7 +91,7 @@ const Login = () => {
                                         <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(_login)}>
                                             <div>
                                                 <label for="email" className="block mb-2 text-sm font-mediumtext-white">Your email</label>
-                                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" 
+                                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" 
                                                 {...register("email", {
                                                     required: true,
                                                     validate: {
